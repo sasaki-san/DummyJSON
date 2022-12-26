60dec8b8df56ff3a5aa248c4709364f0167b502b
Yu Sasaki <ysasaki.g.drive@gmail.com>
2022-12-27 00:39:25 +0900

    add documentation link

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,16 @@ const users = require('./users');
 
 router.use('/users', users);
 router.use('/user', user);
+router.use('/', (req, res) => {
+  res.send(`
+<html>
+<body>
+
+<a href='https://documenter.getpostman.com/view/21861725/VUxNRnau#5d7760e0-15dc-46da-9c3d-0a1eb39b57db'>API Documentation</a>
+
+</body>
+</html>  
+  `)
+})
 
 module.exports = router;
